137e725de4ab0c9eff396f103fd9398570edd7c0
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-09-15 21:00:09 +0545

    chore: Add Next.js configuration files and update dependencies

---
+++ translator/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+ 
+import { AuroraBackgroundDemo } from "@/components/ui/Aurora-bg";
+import React from "react";
+ 
+export default function Home() {
+  return (
+    <div>
+      <AuroraBackgroundDemo />
+    </div>
+  );
+}
