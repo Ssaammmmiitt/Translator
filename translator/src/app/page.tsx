b0473a946f7c78626b950ad4e2799fb77a6125a6
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-09-15 21:39:07 +0545

    refactor: Update package dependencies and add simplex-noise library
    - Update package-lock.json to include the simplex-noise library version 4.0.3
    - Add simplex-noise library to package.json dependencies
    - Remove unused Aurora-bg component and its related styles
    - Add new WavyBackground component for creating wavy backgrounds
    - Update Home component to use the new WavyBackground component
    
    Closes #123

--- translator/src/app/page.tsx
+++ translator/src/app/page.tsx
@@ -1,12 +1,22 @@
 "use client";
  
-import { AuroraBackgroundDemo } from "@/components/ui/Aurora-bg";
 import React from "react";
+import { WavyBackground } from "@/components/ui/wavy-background";
+
  
 export default function Home() {
   return (
-    <div>
-      <AuroraBackgroundDemo />
-    </div>
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center">
+      <div className="relative overflow-hidden h-screen">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 py-10 sm:py-24">
+          <div className="text-center">
+            
+            <h1 className="text-4xl text-white">TransLator</h1>
+          </div>
+        </div>
+      </div>
+      </div>
+    </WavyBackground>
   );
 }
--- translator/tailwind.config.ts
+++ translator/tailwind.config.ts
@@ -65,14 +65,6 @@ const config = {
       },
 
       keyframes: {
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
-        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -83,7 +75,6 @@ const config = {
         },
       },
       animation: {
-        aurora: "aurora 60s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
@@ -92,19 +83,8 @@ const config = {
   plugins: [
     require("tailwindcss-animate"),
     require("preline/plugin"),
-    addVariablesForColors,
   ],
 } satisfies Config;
 
 export default config;
 
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
